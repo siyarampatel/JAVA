b3339192dfae65b3744a384e84246145cce1d691
siyarampatel <siyarampateljbp@gmail.com>
2025-11-03 12:57:25 +0530

    code

---
+++ class1.java
@@ -0,0 +1,21 @@
+// class class1{
+//     public static void main(String[] args){
+//         System.out.println("java");
+//     }
+// }
+
+// class class1{
+//     public static void main(String[] args){
+//         byte a=5;
+//         short b=5;
+//         int c=5;
+//         float f=90.3f;
+//         boolean data = false; //1 bit of memory either true or false (0 or 1)
+//         System.out.println(a);
+//         System.out.println(b);
+//         System.out.println(c);
+//         System.out.println(f);
+//         System.out.println(data);
+//     }
+// }
+
