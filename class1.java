8d9e62c3232be215f57c2396428c7bb7018d7c41
siyarampatel <siyarampateljbp@gmail.com>
2025-11-03 14:03:47 +0530

    add

--- class1.java
+++ class1.java
@@ -18,4 +18,72 @@
 //         System.out.println(data);
 //     }
 // }
+// import java.util.Scanner;
+// class class1{
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter your number: ");
+//         int a = sc.nextInt();
+//         if(a%2==0){
+//             System.out.println("The number is even");
+//         }else{
+//             System.out.println("The number is odd");
+//         }
+//     }
+// }
 
+//Prime number code
+// import java.util.Scanner;
+// class class1{
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter any number: ");
+//         int n = sc.nextInt();
+//         boolean isPrime = true;
+//         if(n<2){
+//             isPrime = false;
+//         }
+//         for(int i=2; i<n/2; i++){
+//             if(n%i==0){
+//                 isPrime = false;
+//             }
+//         }
+//         if(isPrime){
+//             System.out.println("It's an prime number");
+//         }else{
+//             System.out.println("Not an prime number");
+//         }
+//     }
+// }
+
+// Type casting 
+// class class1{
+//     public static void main(String [] args){
+// int a = 600;
+// byte b = a;
+// System.out.println(b);
+//     }
+
+//Palindrome checker for an integer
+import java.util.Scanner;
+class class1{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int original = n;
+            int reversed = 0;
+        
+        while(n!=0){
+            int digit = n%10;
+            reversed = reversed*10+digit;
+            n=n/10;
+        }
+        if(original==reversed){
+            System.out.println("It's an pallindrome");
+        }
+        else{
+            System.out.println("Not an pallindrome");
+        }
+        sc.close();
+    }
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+class class1{
+//     public static void main(String [] args){
+// int a = 600;
+// byte b = a;
+// System.out.println(b);
+//     }
